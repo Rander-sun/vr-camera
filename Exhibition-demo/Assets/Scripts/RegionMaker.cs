20c234b2551c4964a04482a4acef6b0905d693f3
Runder <823504052@qq.com>
2022-03-03 15:25:42 +0800

    make comments

--- Exhibition-demo/Assets/Scripts/RegionMaker.cs
+++ Exhibition-demo/Assets/Scripts/RegionMaker.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class RegionMaker : MonoBehaviour
 {
-    public Transform plane;
-    public Transform centers;
+    public Transform plane;//ground
+    public Transform centers;//spheres
     public float x;
     public float y;
     public List<Region> regions = new List<Region>();
@@ -16,22 +16,25 @@ public class RegionMaker : MonoBehaviour
     {
         // transform为人物模型的MeshRenderer的transform
         var size = plane.GetComponent<Renderer>().bounds.size;
-        Debug.Log ("x: " + size.x + ",y: " + size.z);
-        Vector3 startPos = plane.position - new Vector3(size.x / 2, 0, size.z / 2);
+        Debug.Log ("x: " + size.x + ",y: " + size.z);//Length and width of the ground
+        Vector3 startPos = plane.position - new Vector3(size.x / 2, 0, size.z / 2);//Take the lower left corner as the origin
         for (float i = x/2; i < size.x; i += x)
         {
             for (float j = y/2; j < size.z; j += y)
             {
-                Debug.Log("循环一次！");
+                //Blocking
+                Debug.Log("A loop！");
                 Region region = new Region(x, y, startPos+new Vector3(i, size.y,j));
-                //Region region = new Region();
                 regions.Add(region);
                 Debug.Log("pos: " + region.pos);
+                
+                //Generating the central sphere
                 var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                 sphere.GetComponent<Renderer>().material.color=Color.red;
                 sphere.transform.position = region.pos;
                 sphere.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
                 sphere.transform.parent = centers;
+                
             }
         }
         // for (float i = 1f; i < 5f; i++)
