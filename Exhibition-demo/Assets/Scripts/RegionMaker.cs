93e49befa2ec7f6fa50ee3cbc17061716f3bb17f
Runder <823504052@qq.com>
2022-03-05 12:20:59 +0800

    check agents

--- Exhibition-demo/Assets/Scripts/RegionMaker.cs
+++ Exhibition-demo/Assets/Scripts/RegionMaker.cs
@@ -10,6 +10,7 @@ public class RegionMaker : MonoBehaviour
     public float x;
     public float y;
     public List<Region> regions = new List<Region>();
+    public Transform agents;
     
     // Start is called before the first frame update
     void Start()
@@ -22,7 +23,7 @@ public class RegionMaker : MonoBehaviour
             for (float j = y/2; j < size.z; j += y)
             {
                 //Blocking
-                Debug.Log("A loop！");
+                //Debug.Log("A loop！");
                 Region region = new Region(x, y, startPos+new Vector3(i, size.y,j));
                 regions.Add(region);
                 Debug.Log("pos: " + region.pos);
@@ -33,7 +34,6 @@ public class RegionMaker : MonoBehaviour
                 sphere.transform.position = region.pos;
                 sphere.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
                 sphere.transform.parent = centers;
-                
             }
         }
         // for (float i = 1f; i < 5f; i++)
@@ -48,6 +48,29 @@ public class RegionMaker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        checkAgent();
+    }
+
+    void checkAgent()
+    {
+        int count = 1;
+        foreach (var region in regions)
+        {
+            foreach (Transform child in agents)
+            {
+                var pos = child.position;
+                float x_max = region.pos.x + region.x / 2;
+                float x_min = region.pos.x - region.x / 2;
+                float y_max = region.pos.z + region.y / 2;
+                float y_min = region.pos.z - region.y / 2;
+                if (pos.x >= x_min && pos.x <= x_max && pos.z >= y_min && pos.z <= y_max)
+                {
+                    Debug.Log("region "+count+"检测到角色！");
+                }
+            }
+
+            count++;
+        }
         
     }
 }
